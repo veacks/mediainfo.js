f129216215c6256a1ca48c6be28a0642f45fe4fc
Valentin Dubois <veacks.net@gmail.com>
2019-06-15 06:07:00 +0200

    add web worker support

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ em++ ${OPTS} --llvm-lto 0  -s TOTAL_MEMORY=1073741824 -s NO_FILESYSTEM=1 -s MODU
 mv mediainfo.js mediainfo_.js
 cat pre.js mediainfo_.js post.js > mediainfo.js
 rm mediainfo_.js
+
+em++ ${OPTS} -DUNICODE -std=c++11 -I MediaInfoLib/Source -I ZenLib/Source --bind -c mediainfojs.cpp && \
+em++ ${OPTS} --llvm-lto 0  -s TOTAL_MEMORY=1073741824 -s NO_FILESYSTEM=1 -s BUILD_AS_WORKER=1 --bind mediainfojs.o MediaInfoLib/Project/GNU/Library/.libs/libmediainfo.a ZenLib/Project/GNU/Library/.libs/libzen.a Shared/Source/zlib/libz.a -o mediainfo_worker.js
---
+++ copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp mediainfo.js ./build/mediainfo.js
+cp mediainfo.wasm ./build/mediainfo.wasm
+cp mediainfo_worker.js ./build/mediainfo_worker.js
+cp mediainfo_worker.wasm ./build/mediainfo_worker.wasm
+cp mediainfojs.o ./build/mediainfojs.o
